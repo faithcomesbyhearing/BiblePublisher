9288b882b0252f56d0a53bcae46a01f6550ede04
Gary N Griswold <gary@shortsands.com>
2020-12-29 23:09:07 -0500

    Fix bug in ensureDirectory, to handle full pathnames.

--- library/util/Directory.js
+++ library/util/Directory.js
@@ -2,13 +2,17 @@
 var fs = require('fs');
 var ensureDirectory = function(fullpath, callback) {
 	var path = fullpath.split('/');
+	if (path[0] === "") {
+		path.shift();
+		path[0] = "/" + path[0];
+	}
 	var dir = path.shift();
 	ensureDirPart(dir, path);
 	
 	function ensureDirPart(dir, path) {
 		fs.lstat(dir, function(err, stat) {
 			if (err) {
-				console.log('mkdir', dir);
+				//console.log('mkdir', dir);
 				fs.mkdirSync(dir);
 			}
 			var next = path.shift();
@@ -21,3 +25,8 @@ var ensureDirectory = function(fullpath, callback) {
 		});
 	}
 };
+//ensureDirectory("/Volumes/FCBH/BiblePublisher/ENGWEB/output/xml", function() {
+//	ensureDirectory("BobTestDir", function() {
+//		console.log("Directory Created.")
+//	});
+//})
