f2ba52767c90ced088366f2b58d54b3b0076dd66
Gary N Griswold <gary@shortsands.com>
2021-03-09 23:34:50 -0500

    Change regex to more fully remove eid elements for comparison.

--- validate/HTML.js
+++ validate/HTML.js
@@ -313,7 +313,7 @@ function USXFileCompare(originalDir, generatedDir, filename, testType) {
 			}
 			if (testType == "HTML") {
 				original = original.replace(/\"\/\>/g, '" />');  // Hack to fix no space in some empty elements
-				original = original.replace(/<verse eid=\"[A-Z0-9\, :-]+\" \/>/g, ''); // Hack remove eid elements
+				original = original.replace(/<verse eid=\"[A-Z0-9a-z\, :-]+\" \/>/g, ''); // Hack remove eid elements
 				original = original.replace(/ vid=\"[A-Z0-9 :-]+\"/g, '');
 				original = original.replace(/optbreak \/>/g, 'optbreak/>'); // Hack to fix no space in optbreak element.
 				original = original.replace(/ vid=\"[A-Z0-9]{3} [0-9]+\:[0-9a-d]+\"/g, '')
